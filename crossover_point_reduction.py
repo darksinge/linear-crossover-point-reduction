c11b966bd922d60d60c42ce7f04d0a5106b9669f
Craig Blackburn <cr.blackburn89@gmail.com>
2019-01-26 11:05:24 -0700

    update crossover_point_reduction.py
    Fixes issues with the main algorithm.

--- crossover_point_reduction.py
+++ crossover_point_reduction.py
@@ -6,6 +6,7 @@ the original vector in as few data points as possible.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from operator import xor
 
 
 class LinearCrossoverPointReduction(object):
@@ -71,40 +72,24 @@ class LinearCrossoverPointReduction(object):
         # uo = vector of under-over points
         ou, uo = self.get_crossover_points(v)
 
-        u = np.empty(v.shape, dtype=float)
-        u[0] = v[0]
+        u = v.copy()
 
         for i in range(1, len(v)):
-            ou1 = ou[i-1]
-            ou2 = ou[i]
-            a1 = v[i-1]
-            a2 = v[i]
 
-            ou_intersects = self._point_of_intersection(ou1, ou2, a1, a2)
-            if self._target_in_bounds(ou_intersects[0], ou1[0], ou2[0], a1[0], a2[0]):
-                u[i] = ou_intersects
+            ou_ipoint = self._point_of_intersection(ou[i-1], ou[i], v[i-1], v[i])
+            uo_ipoint = self._point_of_intersection(uo[i-1], uo[i], v[i-1], v[i])
+
+            ou_intersects = self._target_in_bounds(ou_ipoint[0], ou[i-1][0], ou[i][0], v[i-1][0], v[i][0])
+            uo_intersects = self._target_in_bounds(uo_ipoint[0], uo[i-1][0], uo[i][0], v[i-1][0], v[i][0])
+
+            assert not(ou_intersects and uo_intersects)
+
+            if ou_intersects:
+                u[i] = ou_ipoint
+            elif uo_intersects:
+                u[i] = uo_ipoint
             else:
-                # x = (ou[i-1][0] + uo[i-1][0]) / 2.0
-                # y = (ou[i-1][1] + uo[i-1][1]) / 2.0
-                # u[i] = (x, y)
-                u[i] = None
-
-        for i in range(1, len(v)-1):
-            uo1 = uo[i-1]
-            uo2 = uo[i]
-            b1 = v[i]
-            b2 = v[i+1]
-
-            uo_intersects = self._point_of_intersection(uo1, uo2, b1, b2)
-            if self._target_in_bounds(uo_intersects[0], uo1[0], uo2[0], b1[0], b2[0]):
-                # assert u[i+1] is None
-                u[i+1] = uo_intersects
-            else:
-                x = (ou[i][0] + uo[i][0]) / 2.0
-                y = (ou[i][1] + uo[i][1]) / 2.0
-                u[i+1] = (x, y)
-
-        assert len(u) == len(v)
+                u[i] = v[i]
 
         if iterations:
             return self.cross_reduce(u, iterations - 1)
@@ -115,24 +100,20 @@ class LinearCrossoverPointReduction(object):
         lenv = len(v)
 
         uo = np.empty(v.shape, dtype=float)
-        for i in range(1, lenv):
-            if self._slope(v[i], v[i - 1]) != 0:
-                x = v[i][0]
-                y = v[i-1][1]
-                uo[i-1] = (x, y)
-            else:
-                uo[i-1] = (v[i][0], v[i][1])
-        uo[-1] = (v[-1][0]+1, v[-1][1])
-
         ou = np.empty(v.shape, dtype=float)
-        for i in range(0, lenv-1):
-            if self._slope(v[i], v[i+1]) != 0:
-                x = v[i][0]
-                y = v[i+1][1]
-                ou[i] = (x, y)
+
+        for i in range(1, lenv):
+
+            if self._slope(v[i-1], v[i]):
+                uo[i-1] = (v[i][0], v[i-1][1])
+                ou[i-1] = (v[i-1][0], v[i][1])
+
             else:
-                ou[i] = (v[i][0], v[i][1])
-        ou[-1] = v[-1]  # "black magic"
+                uo[i-1] = v[i]
+                ou[i-1] = v[i-1]
+
+        uo[lenv-1] = (v[-1][0]+1, v[-1][1])
+        ou[lenv-1] = v[-1]
 
         return ou, uo
 
@@ -141,13 +122,12 @@ class LinearCrossoverPointReduction(object):
 
 
 if __name__ == "__main__":
-    v = [0, 9, 6, 6, 5, 6, 6, 6, 7, 6, 6, 5, 0]
-    v = [p for p in zip(range(len(v)), v)]
-    v = np.array(v, dtype=(float, 2))
+    y_vals = [0, 9, 6, 6, 5, 6, 6, 6, 7, 6, 6, 5, 0]
+    v = np.array([p for p in zip(range(len(y_vals)), y_vals)], dtype=(float, 2))
 
     lcpr = LinearCrossoverPointReduction()
 
-    u = lcpr.cross_reduce(v, iterations=10)
+    u = lcpr.cross_reduce(v, iterations=100)
 
     plt.plot([x[0] for x in v], [x[1] for x in v], 'b')
     plt.plot([x[0] for x in u], [x[1] for x in u], 'r')
